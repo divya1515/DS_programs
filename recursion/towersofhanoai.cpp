ba4df2e76d2617a556829cc9a8e886bfb67e47e1
divya1515 <114672831+divya1515@users.noreply.github.com>
2022-10-13 14:41:39 +0530

    Create towersofhanoai.cpp

---
+++ recursion/towersofhanoai.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+void towersofhanoai(int n,char s,char m,char d)
+{
+     if(n==1)
+     {
+        cout<<s<<"-"<<d<<endl;
+     }
+     else
+     {
+        towersofhanoai(n-1,s,d,m);
+        cout<<s<<"-"<<d<<endl;
+        towersofhanoai(n-1,m,s,d);
+     }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    char s,m,d;
+    towersofhanoai(n,'s','m','d');
+}
