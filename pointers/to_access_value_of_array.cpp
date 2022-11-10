197f401058f8a07643b22236f8d1d0502d75b0ea
divya1515 <114672831+divya1515@users.noreply.github.com>
2022-11-10 09:30:36 +0530

    Create to_access_value_of_array.cpp

---
+++ pointers/to_access_value_of_array.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+int main(){
+
+   int a[4];
+   int *ptr=a;
+   for(int i=0;i<4;i++)
+   {
+    cin>>a[i];
+   }
+   for(int i=0;i<4;i++)
+   {
+       cout<<*ptr+i;
+   }
+   cout<<"\n";
+   for(int i=0;i<4;i++)
+   {
+       cout<<*a+i;
+   }
+}
