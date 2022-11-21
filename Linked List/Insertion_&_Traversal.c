7d6bdcf9570bcb248af2010c0fc32d20a05bc355
divya1515 <114672831+divya1515@users.noreply.github.com>
2022-11-21 11:39:38 +0530

    Update Insertion_&_Traversal.c

--- Linked List/Insertion_&_Traversal.c
+++ Linked List/Insertion_&_Traversal.c
@@ -1,81 +1,110 @@
-#include<stdlib.h>
-#include<stdio.h>
-struct node{
-  int info;
-  struct node *next;
+#include <stdlib.h>
+#include <stdio.h>
+struct node
+{
+   int info;
+   struct node *next;
 };
 struct node *GetNode()
 {
-    struct node *p;
-    p=(struct node*)malloc(sizeof(struct node));
-    return p;
+   struct node *p;
+   p = (struct node *)malloc(sizeof(struct node));
+   return p;
 }
-void InsertBeg(struct node **start,int x)
+void InsertBeg(struct node **start, int x)
 {
-    struct node *p;
-    p=GetNode();
-    p->info=x;
-    p->next=*start;
-    *start=p;
+   struct node *p;
+   p = GetNode();
+   p->info = x;
+   p->next = *start;
+   *start = p;
 }
-void Insertend(struct node **start,int x)
+void Insertend(struct node **start, int x)
 {
    struct node *q;
-   q=*start;
-   while(q->next!=NULL)
+   q = *start;
+   while (q->next != NULL)
    {
-    q=q->next;
+      q = q->next;
    }
    struct node *p;
-   p=GetNode();
-   p->info=x;
-   p->next=NULL;
-   q->next=p;
+   p = GetNode();
+   p->info = x;
+   p->next = NULL;
+   q->next = p;
 }
 void traverse(struct node *start)
 {
    struct node *p;
-   p=start;
-   while(p!=NULL)
+   p = start;
+   while (p != NULL)
    {
-    printf("%d  ",p->info);
-    p=p->next;
+      printf("%d  ", p->info);
+      p = p->next;
    }
    printf("\n");
 }
-void InsertAfter(struct node *q,int x)
+int count(struct node *start)
 {
-  struct node*r;
-  struct node*p;
-  r=q->next;
-  p=GetNode();
-  p->info=x;
-  p->next=r;
-  q->next=p;
+   int c = 0;
+   struct node *p;
+   p = start;
+   while (p != NULL)
+   {
+      c++;
+      p = p->next;
+   }
+   return c;
+}
+void InsertAfter(struct node *q, int x)
+{
+   struct node *r;
+   struct node *p;
+   r = q->next;
+   p = GetNode();
+   p->info = x;
+   p->next = r;
+   q->next = p;
 }
 int main()
 {
    struct node *start;
-   start=NULL;
-   InsertBeg(&start,200);
-   InsertBeg(&start,300);
-   InsertBeg(&start,400);
-   InsertBeg(&start,500);
-   InsertBeg(&start,600);
-   Insertend(&start,10);
-   Insertend(&start,20);
-   Insertend(&start,30);
+   start = NULL;
+   InsertBeg(&start, 200);
+   InsertBeg(&start, 300);
+   InsertBeg(&start, 400);
+   InsertBeg(&start, 500);
+   InsertBeg(&start, 600);
+   Insertend(&start, 10);
+   Insertend(&start, 20);
+   Insertend(&start, 30);
    printf("Linked list before insertion\n");
    traverse(start);
-   struct node*p=start;
+   struct node *p = start;
    int d;
-   scanf("%d",&d);//position after which the no. to be inserted
-   for(int i=1;i<d;i++)
+   scanf("%d", &d); // position at which the no. to be inserted
+   int c = count(start);
+   int flag = 0;
+   if (d == 1)
    {
-      p=p->next;
+      InsertBeg(&start, 67);
    }
-   InsertAfter(p,67);//no. to be inserted is 67
-  printf("Linked list after insertion\n");
-   traverse(start);
-
+   else if (d > c + 1)
+   {
+      printf("Void Insertion\n");
+      flag = 1;
+   }
+   else
+   {
+      for (int i = 1; i < d - 1; i++)
+      {
+         p = p->next;
+      }
+      InsertAfter(p, 67); // no. to be inserted is 67
+   }
+   if (flag == 0)
+   {
+      printf("Linked list after insertion\n");
+      traverse(start);
+   }
 }
