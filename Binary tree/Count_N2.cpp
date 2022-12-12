8d0db3feff11bc86b89fc40f778901471cc35eca
divya1515 <114672831+divya1515@users.noreply.github.com>
2022-12-12 12:21:13 +0530

    Create Count_N2.cpp

---
+++ Binary tree/Count_N2.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+int CountN2(Node *&root)//parent with 2 children
+{
+    if (root == NULL)
+        return 0;
+    if(root->left!=NULL && root->right!=NULL)
+      return 1+CountN2(root->left)+CountN2(root->right);
+    return CountN2(root->left)+CountN2(root->right);
+
+}
+int main()
+{
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(5);
+    root->right->left = new Node(4);
+    root->right->left->right = new Node(7);
+    root->right->right = new Node(6);
+    cout << CountN2(root) << "\n";
+}
