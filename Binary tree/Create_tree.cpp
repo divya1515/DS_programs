6ee267ecbccb0afcc56b4a6de4b17b978214f2b5
divya1515 <114672831+divya1515@users.noreply.github.com>
2022-12-12 11:59:38 +0530

    Create Create_tree.cpp

---
+++ Binary tree/Create_tree.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+void PreorderTraversal(Node *&root)
+{
+    if (root != NULL)
+    {
+        cout << root->data << " ";
+        PreorderTraversal(root->left);
+        PreorderTraversal(root->right);
+    }
+}
+void InorderTraversal(Node *&root)
+{
+    if (root != NULL)
+    {
+        InorderTraversal(root->left);
+        cout << root->data << " ";
+        InorderTraversal(root->right);
+    }
+}
+void PostorderTraversal(Node *&root)
+{
+    if (root != NULL)
+    {
+        PostorderTraversal(root->left);
+        PostorderTraversal(root->right);
+        cout << root->data << " ";
+    }
+}
+void CreateTree(Node *&root)
+{
+    int ch;
+   cout<<"Enter choice whether left of "<<root->data<<" exist or not(1/0)"<<"\n";
+   cin>>ch;//if left node exist then ch=1 else ch=0
+   if(ch==1)
+   {
+      int d;
+      cout<<"Enter left data for "<<root->data<<": ";
+      cin>>d;
+      root->left=new Node(d);
+      CreateTree(root->left);
+   }
+   cout<<"Enter choice whether right of "<<root->data<<" exist or not(1/0)"<<"\n";
+   cin>>ch;//if right node exist then ch=1 else ch=0
+   if(ch==1)
+   {
+     int d;
+     cin>>d;
+     cout<<"Enter right data for "<<root->data<<": ";
+     root->right=new Node(d);
+     CreateTree(root->right);
+   }
+
+}
+int main()
+{
+    Node *root = new Node(1);
+    CreateTree(root);
+    cout<<"PreorderTraversal: "<<"\n";
+    PreorderTraversal(root);
+    cout<<endl;
+    cout<<"InorderTraversal: "<<"\n";
+    InorderTraversal(root);
+    cout<<endl;
+    cout<<"PostorderTraversal: "<<"\n";
+    PostorderTraversal(root);
+    cout<<endl;
+}
