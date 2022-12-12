41ce753de5a18e0bc4b96b4a7d057b6843817270
divya1515 <114672831+divya1515@users.noreply.github.com>
2022-12-12 10:23:58 +0530

    Create Traversal.cpp

---
+++ Binary tree/Traversal.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+void PreorderTraversal(Node *&root)
+{
+    if (root != NULL)
+    {
+        cout << root->data << " ";
+        PreorderTraversal(root->left);
+        PreorderTraversal(root->right);
+    }
+}
+void InorderTraversal(Node *&root)
+{
+    if (root != NULL)
+    {
+        InorderTraversal(root->left);
+        cout << root->data << " ";
+        InorderTraversal(root->right);
+    }
+}
+void PostorderTraversal(Node *&root)
+{
+    if (root != NULL)
+    {
+        PostorderTraversal(root->left);
+        PostorderTraversal(root->right);
+        cout << root->data << " ";
+    }
+}
+int main()
+{
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(5);
+    root->right->left = new Node(4);
+    root->right->left->right = new Node(7);
+    root->right->right = new Node(6);
+    PreorderTraversal(root);
+    cout << endl;
+    InorderTraversal(root);
+    cout << endl;
+    PostorderTraversal(root);
+}
