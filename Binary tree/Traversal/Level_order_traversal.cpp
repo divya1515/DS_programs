34d9a8ee16bbb20601ebe81923ce71c6d9086908
divya1515 <114672831+divya1515@users.noreply.github.com>
2022-12-18 22:59:02 +0530

    Update Level_order_traversal.cpp

--- Binary tree/Traversal/Level_order_traversal.cpp
+++ Binary tree/Traversal/Level_order_traversal.cpp
@@ -1,4 +1,68 @@
 #include <bits/stdc++.h>
+#include <queue>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+void Level_order_traversal(Node *&root)
+{
+    queue<Node *> q;
+    Node *p = root;
+    q.push(p);
+    q.push(NULL);
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        q.pop();
+        if (temp == NULL)
+        {
+            cout << endl;
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            cout << temp->data;
+            if (temp->left != NULL)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right != NULL)
+                q.push(temp->right);
+        }
+    }
+}
+
+int main()
+{
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    Level_order_traversal(root);
+}
+
+
+<------------------------------------------------------------------------------------------------------------------------------------------------------------------>
+    
+    
+#include <bits/stdc++.h>
 using namespace std;
 class Node
 {
