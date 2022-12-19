6887714b96d48c8e074c56063bad3825f3136b14
divya1515 <114672831+divya1515@users.noreply.github.com>
2022-12-19 11:45:39 +0530

    Create Successor.cpp

---
+++ Binary tree/Binary_search_tree/Successor.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+   int data;
+   Node *left;
+   Node *right;
+   Node *father;
+   Node(int data)
+   {
+      this->data = data;
+      this->left = NULL;
+      this->right = NULL;
+      this->father=NULL;
+   }
+};
+void InorderTraversal(Node *&root)
+{
+   if (root != NULL)
+   {
+      InorderTraversal(root->left);
+      cout << root->data << " ";
+      InorderTraversal(root->right);
+   }
+}
+void InsertBinary_Search_tree(Node *&root, int x)
+{
+   if (root == NULL)
+   {
+      root = new Node(x);
+      return;
+   }
+
+   Node *p = NULL;
+   Node *q = root;
+   while (q != NULL)
+   {
+      p = q;
+      if (x < q->data)
+      {
+         q = q->left;
+      }
+      else
+      {
+         q = q->right;
+      }
+   }
+   if (x < p->data)
+   {
+      p->left = new Node(x);
+      p->left->father=p;
+   }
+   else
+   {
+      p->right = new Node(x);
+      p->right->father=p;
+   }
+}
+Node *Binary_Search(Node *&root,int x)
+{
+   while(root!=NULL)
+   {
+   if(root->data==x)
+   {
+      return root;
+   }
+   if(root->data <x)
+   {
+      root=root->right;
+   }
+   else
+     root=root->left;
+   }
+   return NULL;
+} 
+Node* Minimum(Node *&root)
+{
+   Node *p=root;
+   while(p->left!=NULL)
+   {
+      p=p->left;
+   }
+   return p;
+}
+void Successor(Node *p)
+{
+   Node *q;
+  if(p->right!=NULL)
+  {
+     q=Minimum(p->right);
+  }
+  else
+  {
+    q=p->father;
+     while(q!=NULL && q->right==p)
+     {
+       p=q;
+       q=q->father;
+     }
+  }
+  cout<<q->data;
+}
+int main()
+{
+   Node *root = NULL;
+   InsertBinary_Search_tree(root, 500);
+   InsertBinary_Search_tree(root, 200);
+   InsertBinary_Search_tree(root, 10);
+   InsertBinary_Search_tree(root, 20);
+   InsertBinary_Search_tree(root, 60);
+   InsertBinary_Search_tree(root, 300);
+   InorderTraversal(root);
+   cout<<endl;
+   int x;
+   cin>>x;
+   Node *f=Binary_Search(root,x);
+   Successor(f);
+}
