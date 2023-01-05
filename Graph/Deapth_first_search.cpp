d6b77157ba25d9f2a6db5c18469c4bfbc85f3a57
divya1515 <114672831+divya1515@users.noreply.github.com>
2023-01-05 09:55:33 +0530

    Update Deapth_first_search.cpp

--- Graph/Deapth_first_search.cpp
+++ Graph/Deapth_first_search.cpp
@@ -1,16 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-void DFS_visit(int i,vector<int>&visited,vector<int>AdjList[],vector<int>&pred)
+void DFS_visit(int i,vector<int>&visited,vector<int>AdjList[],vector<int>&pred,int &elements)
 {
     cout<<i<<" "<<pred[i];// dfs with its predeccessor
     cout<<"\n";
     visited[i]=1;
+    elements++;
     for(int j=0;j<AdjList[i].size();j++)
     {
         if(visited[AdjList[i][j]]==0)
         {
             pred[AdjList[i][j]]=i;
-            DFS_visit(AdjList[i][j],visited,AdjList,pred);
+            DFS_visit(AdjList[i][j],visited,AdjList,pred,elements);
         }
     }
 }
@@ -19,12 +20,24 @@ void DFS(vector<int>AdjList[],int v)
    vector<int>visited(v);
    vector<int>dis(v);
    vector<int>pred(v,-1);
+   int comp=0;
+   vector<int>vec;
    for(int i=0;i<v;i++)
    {
       if(visited[i]==0)
       {
-        DFS_visit(i,visited,AdjList,pred);
+        comp++;
+        int elements=0;
+        DFS_visit(i,visited,AdjList,pred,elements);
+        vec.push_back(elements);
       }
+   }
+   cout<<"\n";
+   cout<<"No of components"<<comp;
+   cout<<endl;
+   for(int i=0;i<vec.size();i++)
+   {
+    cout<<vec[i]<<" ";
    }
 
 }
