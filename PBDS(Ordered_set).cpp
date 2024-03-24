2d95e025f57c0e0b4249e96995b83e44eea2d5b5
divya1515 <114672831+divya1515@users.noreply.github.com>
2024-03-24 15:14:00 +0530

    Update PBDS(Ordered_set).cpp

--- PBDS(Ordered_set).cpp
+++ PBDS(Ordered_set).cpp
@@ -6,6 +6,7 @@ using namespace __gnu_pbds;
 //find_by_order and order_of_key
 typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> pbds; //like set in ascending order
 // typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update> pbds; //for multiset in ascending order
+// typedef tree<int,null_type,greater_equal<int>,rb_tree_tag,tree_order_statistics_node_update> pbds;//multiset in descending order
 void solve(){
     pbds A;
     A.insert(1);
